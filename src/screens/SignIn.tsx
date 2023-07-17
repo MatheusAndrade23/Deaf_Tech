fcc3b0bdc2292c2782ad6031192d6b4c917a6f19
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-07-16 23:07:23 -0300

    Sign In Screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,59 @@
-import { Box } from 'native-base';
+import { ScrollView, Center, useTheme, Image, Text, VStack } from 'native-base';
+
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+
+import {
+  User,
+  LockSimple,
+  EnvelopeSimple,
+  UserPlus,
+} from 'phosphor-react-native';
+
+import Logo from '@assets/logo.png';
 
 export const SignIn = () => {
-  return <Box flex={1} />;
+  const { colors } = useTheme();
+
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <Center flex={1} px="8">
+        <Image source={Logo} alt="Logotipo" w="250" h="180" />
+        <Text color="secondaryColor" fontFamily="heading" fontSize="md" mb="4">
+          Acesse sua conta
+        </Text>
+        <Input
+          title="Email:"
+          placeholder="Digite seu email"
+          icon={<EnvelopeSimple color={colors.gray.tertiary} />}
+        />
+        <Input
+          title="Senha:"
+          placeholder="Digite sua senha"
+          icon={<LockSimple color={colors.gray.tertiary} />}
+        />
+        <Button
+          text="Entrar"
+          mt="4"
+          icon={<User color={colors.secondaryColor} weight="bold" />}
+        />
+
+        <VStack alignItems="center" w="70%" mt="20%">
+          <Text fontFamily="body" color="secondaryColor">
+            Ainda não tem acesso?
+          </Text>
+          <Button
+            text="Criar conta"
+            mt="4"
+            h="12"
+            variant="secondary"
+            icon={<UserPlus color={colors.gray.tertiary} />}
+          />
+        </VStack>
+      </Center>
+    </ScrollView>
+  );
 };
