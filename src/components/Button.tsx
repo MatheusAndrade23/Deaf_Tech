79c981308da877c63a9e518c10e591def5fb95bb
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-07-16 22:11:54 -0300

    Button Component

---
+++ src/components/Button.tsx
@@ -0,0 +1,96 @@
+import { ReactNode } from 'react';
+import {
+  Button as NativeButton,
+  IButtonProps,
+  Text,
+  HStack,
+  Box,
+  useTheme,
+  Center,
+} from 'native-base';
+
+type Props = IButtonProps & {
+  text: string;
+  icon?: ReactNode;
+  variant?: 'default' | 'secondary' | 'tertiary';
+};
+
+export const Button = ({ text, icon, variant = 'default', ...rest }: Props) => {
+  const { colors } = useTheme();
+
+  const buttonStyle = () => {
+    switch (variant) {
+      case 'default': {
+        return {
+          bg: colors.primaryColor,
+          pressed: colors.gray.tertiary,
+          borderColor: null,
+          borderWidth: 0,
+        };
+      }
+
+      case 'secondary': {
+        return {
+          bg: colors.secondaryColor,
+          pressed: colors.gray.secondary,
+          borderColor: null,
+          borderWidth: 0,
+        };
+      }
+
+      case 'tertiary': {
+        return {
+          bg: colors.gray.primary,
+          pressed: colors.gray.tertiary,
+          borderColor: colors.secondaryColor,
+          borderWidth: 1,
+        };
+      }
+    }
+  };
+
+  const textStyle = () => {
+    switch (variant) {
+      case 'secondary': {
+        return {
+          color: colors.gray.tertiary,
+        };
+      }
+
+      default: {
+        return {
+          color: colors.secondaryColor,
+        };
+      }
+    }
+  };
+
+  const style = buttonStyle();
+
+  return (
+    <NativeButton
+      p={0}
+      w="full"
+      h={14}
+      position="relative"
+      borderColor={style?.borderColor}
+      borderWidth={style?.borderWidth}
+      bg={style?.bg}
+      _pressed={{
+        bg: style?.pressed,
+      }}
+      {...rest}
+    >
+      <HStack w="full" h="full" alignItems="center">
+        <Center w="full">
+          <Text fontFamily="heading" fontSize="sm" {...textStyle()}>
+            {text}
+          </Text>
+        </Center>
+        <Box position="absolute" right="4">
+          {icon && icon}
+        </Box>
+      </HStack>
+    </NativeButton>
+  );
+};
