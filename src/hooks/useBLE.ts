1cfaf630d99517ceb8edfe220a524183436314a7
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-07-22 00:25:58 -0300

    useBLE Hook

---
+++ src/hooks/useBLE.ts
@@ -0,0 +1,142 @@
+import { useMemo, useState } from 'react';
+import { PermissionsAndroid, Platform, Linking } from 'react-native';
+import {
+  BleError,
+  BleManager,
+  Characteristic,
+  Device,
+} from 'react-native-ble-plx';
+
+import { AppError } from '@utils/AppError';
+
+import * as ExpoDevice from 'expo-device';
+
+interface BluetoothLowEnergyApi {
+  requestPermissions(): Promise<boolean>;
+  scanForPeripherals(): void;
+  connectToDevice: (deviceId: Device) => Promise<void>;
+  disconnectFromDevice: () => void;
+  connectedDevice: Device | null;
+  allDevices: Device[];
+  checkBluetoothStatus(): Promise<boolean>;
+}
+
+function useBLE(): BluetoothLowEnergyApi {
+  const bleManager = useMemo(() => new BleManager(), []);
+  const [allDevices, setAllDevices] = useState<Device[]>([]);
+  const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
+
+  const checkBluetoothStatus = async (): Promise<boolean> => {
+    try {
+      const state = await bleManager.state();
+      return state === 'PoweredOn';
+    } catch (e) {
+      console.error('Error checking Bluetooth state:', e);
+      return false;
+    }
+  };
+
+  const requestAndroid31Permissions = async () => {
+    const bluetoothScanPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+      {
+        title: 'Permissão de Localização',
+        message: 'Bluetooth requer a acesso à localização',
+        buttonPositive: 'OK',
+      },
+    );
+    const bluetoothConnectPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      {
+        title: 'Permissão de Localização',
+        message: 'Bluetooth requer a acesso à localização',
+        buttonPositive: 'OK',
+      },
+    );
+    const fineLocationPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Permissão de Localização',
+        message: 'Bluetooth requer a acesso à localização',
+        buttonPositive: 'OK',
+      },
+    );
+
+    return (
+      bluetoothScanPermission === 'granted' &&
+      bluetoothConnectPermission === 'granted' &&
+      fineLocationPermission === 'granted'
+    );
+  };
+
+  const requestPermissions = async () => {
+    if (Platform.OS === 'android') {
+      if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Permissão de Localização',
+            message: 'Bluetooth requer a acesso à localização',
+            buttonPositive: 'OK',
+          },
+        );
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } else {
+        const isAndroid31PermissionsGranted =
+          await requestAndroid31Permissions();
+
+        return isAndroid31PermissionsGranted;
+      }
+    } else {
+      return true;
+    }
+  };
+
+  const isDuplicteDevice = (devices: Device[], nextDevice: Device) =>
+    devices.findIndex((device) => nextDevice.id === device.id) > -1;
+
+  const scanForPeripherals = async () =>
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        return error;
+      }
+      if (device && device.name?.includes('CorSense')) {
+        setAllDevices((prevState: Device[]) => {
+          if (!isDuplicteDevice(prevState, device)) {
+            return [...prevState, device];
+          }
+          return prevState;
+        });
+      }
+    });
+
+  const connectToDevice = async (device: Device) => {
+    try {
+      const deviceConnection = await bleManager.connectToDevice(device.id);
+      setConnectedDevice(deviceConnection);
+      await deviceConnection.discoverAllServicesAndCharacteristics();
+      bleManager.stopDeviceScan();
+    } catch (e) {
+      console.log('FAILED TO CONNECT', e);
+    }
+  };
+
+  const disconnectFromDevice = () => {
+    if (connectedDevice) {
+      bleManager.cancelDeviceConnection(connectedDevice.id);
+      setConnectedDevice(null);
+    }
+  };
+
+  return {
+    scanForPeripherals,
+    requestPermissions,
+    connectToDevice,
+    allDevices,
+    connectedDevice,
+    disconnectFromDevice,
+    checkBluetoothStatus,
+  };
+}
+
+export default useBLE;
